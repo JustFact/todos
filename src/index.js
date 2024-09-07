61c1bbc4499fa044eb95e5cb5b3ad4503fbb16fa
Jaspreet Singh <jaspreetsingh6881@gmail.com>
2024-09-07 14:03:09 +0530

    add note to bucketList

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import { todoSymbol, createTodoItem } from "./todo.js";
+import { noteSymbol, createNote } from "./note.js";
 
 function findInstanceOf(element) {
   //this will determine whether the element passed is
   //a todo item, note,
   if (element[todoSymbol]) {
     return "todo";
+  } else if (element[noteSymbol]) {
+    return "note";
   }
 }
 
@@ -15,14 +18,20 @@ function createBucket(title) {
   const addToBucketList = (element) => {
     switch (findInstanceOf(element)) {
       case "todo":
-        console.log("Its a todo item");
+        console.log("It's a todo item");
+        break;
+      case "note":
+        console.log("it's a note");
+        break;
     }
   };
 
   return { title, addToBucketList };
 }
 
+//temporary code to check functionality
 let myBucket = createBucket("myBucket");
 let myTodo = createTodoItem("myTodo", "test", "Nov,11", "1");
-
+let myNote = createNote("myNote", "test");
 myBucket.addToBucketList(myTodo);
+myBucket.addToBucketList(myNote);
