9fdc47093d6a653dd7401df0082286e4ce694e17
Jaspreet Singh <jaspreetsingh6881@gmail.com>
2024-09-07 22:03:44 +0530

    Add toggle for Todo item completion status

--- src/todo.js
+++ src/todo.js
@@ -1,12 +1,18 @@
 export const todoSymbol = Symbol("todo");
 
 export function createTodoItem(title, description, dueDate, priority) {
+  let completeStatus = false;
+  const toggleComplete = () => {
+    completeStatus = !completeStatus;
+  };
+  const isCompleted = () => completeStatus;
   return {
     [todoSymbol]: true,
     title,
     description,
     dueDate,
     priority,
-    isCompleted: false,
+    toggleComplete,
+    isCompleted,
   };
 }
