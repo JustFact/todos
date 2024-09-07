d05e5852d436b2ef71e3de063b4240d52434f03c
Jaspreet Singh <jaspreetsingh6881@gmail.com>
2024-09-07 21:40:50 +0530

    Create separate Bucket module

---
+++ src/bucket.js
@@ -0,0 +1,36 @@
+import { findInstanceOf } from "./utility.js";
+
+export function createBucket(title) {
+  const todo = [];
+  const note = [];
+  const bucket = [todo, note];
+
+  //element could be a todo List, notes or checklist
+  const addToBucket = (element) => {
+    switch (findInstanceOf(element)) {
+      case "todo":
+        bucket[0].push(element);
+        break;
+      case "note":
+        bucket[1].push(element);
+        break;
+    }
+  };
+
+  const deleteFromBucket = (index, element) => {
+    switch (findInstanceOf(element)) {
+      case "todo":
+        bucket[0].splice(index, 1);
+        break;
+      case "note":
+        bucket[1].splice(index, 1);
+        break;
+    }
+  };
+
+  const getBucket = () => {
+    return bucket;
+  };
+
+  return { title, addToBucket, getBucket, deleteFromBucket };
+}
--- src/index.js
+++ src/index.js
@@ -1,41 +1,6 @@
 import { createTodoItem } from "./todo.js";
 import { createNote } from "./note.js";
-import { findInstanceOf } from "./utility.js";
-
-function createBucket(title) {
-  const todo = [];
-  const note = [];
-  const bucket = [todo, note];
-
-  //element could be a todo List, notes or checklist
-  const addToBucket = (element) => {
-    switch (findInstanceOf(element)) {
-      case "todo":
-        bucket[0].push(element);
-        break;
-      case "note":
-        bucket[1].push(element);
-        break;
-    }
-  };
-
-  const deleteFromBucket = (index, element) => {
-    switch (findInstanceOf(element)) {
-      case "todo":
-        bucket[0].splice(index, 1);
-        break;
-      case "note":
-        bucket[1].splice(index, 1);
-        break;
-    }
-  };
-
-  const getBucket = () => {
-    return bucket;
-  };
-
-  return { title, addToBucket, getBucket, deleteFromBucket };
-}
+import { createBucket } from "./bucket.js";
 
 //temporary code to check functionality
 let myBucket = createBucket("myBucket");
@@ -44,5 +9,5 @@ let myNote = createNote("myNote", "test");
 myBucket.addToBucket(myTodo);
 myBucket.addToBucket(myNote);
 myBucket.addToBucket("This won't be added");
-myBucket.deleteFromBucket(0, myNote);
+// myBucket.deleteFromBucket(0, myNote);
 console.log(myBucket.getBucket());
