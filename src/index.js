a236358bca0768852d5c40f7d28bb98ef32e93ad
Jaspreet Singh <jaspreetsingh6881@gmail.com>
2024-09-07 13:44:03 +0530

    reformat the index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-import { todoSymbol,createTodoItem } from "./todo.js";
+import { todoSymbol, createTodoItem } from "./todo.js";
 
-function findInstanceOf(element){
-    //this will determine whether the element passed is
-    //a todo item, note, 
-    if(element[todoSymbol]){
-        return 'todo';
-    }
+function findInstanceOf(element) {
+  //this will determine whether the element passed is
+  //a todo item, note,
+  if (element[todoSymbol]) {
+    return "todo";
+  }
 }
 
-function createBucket (title){
-    const bucketList = [];
+function createBucket(title) {
+  const bucketList = [];
 
-    //element could be a todo List, notes or checklist
-    const addToBucketList = (element)=>{
-
-        switch(findInstanceOf(element)){
-            case 'todo': console.log('Its a todo item');
-        }
+  //element could be a todo List, notes or checklist
+  const addToBucketList = (element) => {
+    switch (findInstanceOf(element)) {
+      case "todo":
+        console.log("Its a todo item");
     }
+  };
 
-    return { title, addToBucketList };
+  return { title, addToBucketList };
 }
 
-let myBucket = createBucket('myBucket');
-let myTodo = createTodoItem('myTodo','test','Nov,11','1');
+let myBucket = createBucket("myBucket");
+let myTodo = createTodoItem("myTodo", "test", "Nov,11", "1");
 
 myBucket.addToBucketList(myTodo);
