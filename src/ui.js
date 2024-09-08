7fc613f57f22c5f3b428623e0a36dc481372dfb3
Jaspreet Singh <jaspreetsingh6881@gmail.com>
2024-09-08 19:24:32 +0530

    create BucketList UI

---
+++ src/ui.js
@@ -0,0 +1,22 @@
+export const getBucketListNavigator = (BucketList) => {
+  const bucketUL = document.createElement("ul");
+  for (let bucket of BucketList) {
+    const bucketLI = document.createElement("li");
+    const subBucketUL = document.createElement("ul");
+    const bucketTitle = document.createElement("li");
+    const subBucketTodos = document.createElement("li");
+    const subBucketNotes = document.createElement("li");
+
+    bucketTitle.innerText = bucket.title;
+    subBucketTodos.innerText = "Todos";
+    subBucketNotes.innerText = "Notes";
+
+    bucketUL.append(bucketTitle);
+    subBucketUL.append(subBucketTodos);
+    subBucketUL.append(subBucketNotes);
+    bucketLI.append(subBucketUL);
+    bucketUL.append(bucketLI);
+  }
+
+  return bucketUL;
+};
