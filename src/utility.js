ee94f3bc9e90ebd4e1922ff44bbd3102d094f648
Jaspreet Singh <40349101+JustFact@users.noreply.github.com>
2024-09-13 04:30:09 +0530

    Fetch data by parsing dataset.index value

--- src/utility.js
+++ src/utility.js
@@ -1,5 +1,6 @@
 import { todoSymbol } from "./todo";
 import { noteSymbol } from "./note";
+import { bucketList } from "./bucketList";
 
 export function findInstanceOf(element) {
   //this will determine whether the element passed is
@@ -8,5 +9,16 @@ export function findInstanceOf(element) {
     return "todo";
   } else if (element[noteSymbol]) {
     return "note";
+  }
+}
+
+export function fetchData(string) {
+  let arr = string.split("#");
+  let bucketData = bucketList[arr[0]].getBucket();
+  switch (arr[1]) {
+    case "todo":
+      return bucketData[0];
+    case "note":
+      return bucketData[1];
   }
 }
