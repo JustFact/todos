4731508ed1411330e2ed6de6f581be4082b01cac
Jaspreet Singh <40349101+JustFact@users.noreply.github.com>
2024-09-15 10:40:48 +0530

    Create getTodoListUI and getNotesUI functions

--- src/ui.js
+++ src/ui.js
@@ -1,3 +1,4 @@
+import { bucketList } from "./bucketList";
 import "./style.css";
 import { fetchData } from "./utility";
 
@@ -39,3 +40,45 @@ export const getBucketListNavigator = (BucketList) => {
 
   return bucketUL;
 };
+
+function getTodoListUI(data) {
+  const todoList = document.createElement("div");
+  todoList.classList.add("todoList");
+  for (let i = 0; i < data.length; i++) {
+    const todoListItem = document.createElement("div");
+    todoListItem.classList.add("todoListItem");
+
+    const todoTitle = document.createElement("h3");
+    const todoDescription = document.createElement("p");
+    const todoDueDate = document.createElement("div");
+    const todoPriority = document.createElement("div");
+
+    todoTitle.innerText = bucketList[0].getBucket()[0][i].title;
+    todoDescription.innerText = bucketList[0].getBucket()[0][i].description;
+    todoDueDate.innerText = bucketList[0].getBucket()[0][i].dueDate;
+    todoPriority.innerText = bucketList[0].getBucket()[0][i].priority;
+
+    todoListItem.append(todoTitle, todoDescription, todoDueDate, todoPriority);
+    todoList.append(todoListItem);
+  }
+  return todoList;
+}
+
+function getNotesUI(data) {
+  const NoteList = document.createElement("div");
+  NoteList.classList.add("NoteList");
+  for (let i = 0; i < data.length; i++) {
+    const NoteListItem = document.createElement("div");
+    NoteListItem.classList.add("NoteListItem");
+
+    const todoTitle = document.createElement("h3");
+    const todoDescription = document.createElement("p");
+
+    todoTitle.innerText = bucketList[0].getBucket()[1][i].title;
+    todoDescription.innerText = bucketList[0].getBucket()[1][i].description;
+
+    NoteListItem.append(todoTitle, todoDescription);
+    NoteList.append(NoteListItem);
+  }
+  return NoteList;
+}
