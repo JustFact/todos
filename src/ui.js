d3ac137a9750e5caefd17c7623bfcddf85c5cd8c
Jaspreet Singh <40349101+JustFact@users.noreply.github.com>
2024-10-02 12:48:23 +0530

    Make todoItem delete button functional

--- src/ui.js
+++ src/ui.js
@@ -124,6 +124,7 @@ function getTodoListUI(data) {
   for (let i = 0; i < data.length; i++) {
     const todoListItem = document.createElement("div");
     todoListItem.classList.add("todoListItem", "collapse");
+    todoListItem.dataset.index = i;
 
     const todoTitle = document.createElement("h4");
     todoTitle.classList.add("todo-title", "collapse");
@@ -151,6 +152,15 @@ function getTodoListUI(data) {
     deleteButton.classList.add("todoDeleteButton");
     deleteButton.innerHTML =
       '<svg class="icon todoDeleteIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>delete-forever</title><path d="M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19M8.46,11.88L9.87,10.47L12,12.59L14.12,10.47L15.53,11.88L13.41,14L15.53,16.12L14.12,17.53L12,15.41L9.88,17.53L8.47,16.12L10.59,14L8.46,11.88M15.5,4L14.5,3H9.5L8.5,4H5V6H19V4H15.5Z" /></svg>';
+
+    deleteButton.addEventListener("click", (e) => {
+      let todoItemIndex = e.currentTarget.parentNode.parentNode.dataset.index;
+      let elementID =
+        document.querySelector(".addTodoItemButton").dataset.index;
+      let bucketIndex = elementID.split("#")[0];
+      bucketList[bucketIndex].deleteFromBucket(todoItemIndex, "todo");
+      displayList(elementID);
+    });
 
     buttonDiv.append(editButton, deleteButton);
 
