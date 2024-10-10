b7fe42c150e4be687e965dd415a253368e22735a
Jaspreet Singh <40349101+JustFact@users.noreply.github.com>
2024-10-10 19:37:49 +0530

    Display the application UI

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import { createTodoItem } from "./todo.js";
-import { createNote } from "./note.js";
-import { createBucket } from "./bucket.js";
+import { displayUI } from "./ui/ui.js";
 
-//temporary code to check functionality
-let myBucket = createBucket("myBucket");
-let myTodo = createTodoItem("myTodo", "test", "Jan,01", "1");
-let myNote = createNote("myNote", "test");
-myBucket.addToBucket(myTodo);
-myBucket.addToBucket(myNote);
-myBucket.addToBucket("This won't be added");
-// myBucket.deleteFromBucket(0, myNote);
-console.log(myBucket.getBucket());
+displayUI();
+
+/*  ######## MUST HAVE ########## 
+
+*/
+
+/*  =======SHOULD HAVE========
+    GIVE TITLE TO TODO/NOTE UI
+    HAVE CLEANER DATE FORMAT
+*/
+
+/*  --------Good To have ------------
+    MAKE NUMBER OF ITEMS APPEAR WITH TODO/NOTES IN NAVIGATOR 
+*/
