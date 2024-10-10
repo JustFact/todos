9422900bedde03a8687354482ed69553d0504a1d
Jaspreet Singh <40349101+JustFact@users.noreply.github.com>
2024-10-10 20:57:59 +0530

    Add My Bucket List Logo

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -43,10 +43,15 @@ export const displayUI = (elementID = 0) => {
   } else {
     const bucketListUI = getBucketListNavigator(bucketList);
     const addBucketButton = getAddBucketButton();
+    const logo = document.createElement("div");
+    logo.classList.add("logo");
+    logo.innerHTML =
+      '<svg class="icon logoIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>My Bucket List</title><path d="M21.03,3L18,20.31C17.83,21.27 17,22 16,22H8C7,22 6.17,21.27 6,20.31L2.97,3H21.03M5.36,5L8,20H16L18.64,5H5.36M9,18V14H13V18H9M13,13.18L9.82,10L13,6.82L16.18,10L13,13.18Z" /></svg> <h3 class="logoText" >My Bucket List</h3>';
+    // logo.append(logoIcon, logoText);
 
     const sidePanel = document.createElement("div");
     sidePanel.classList.add("side-panel");
-    sidePanel.replaceChildren(...[addBucketButton, bucketListUI]);
+    sidePanel.replaceChildren(...[logo, addBucketButton, bucketListUI]);
 
     const addBucketDialog = getAddBucketDialogUI();
     const addTodoItemDialog = getAddTodoDialogUI();
